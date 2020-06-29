935a0587e1b395f87eca1f13a38fc096bb4f5ec8
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-06-29 20:13:02 +0530

    main functionality added

--- app/src/main/java/com/paru/catchthekenny/MainActivity.kt
+++ app/src/main/java/com/paru/catchthekenny/MainActivity.kt
@@ -4,12 +4,19 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
 import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     var score:Int=0
+    var imageArray=ArrayList<ImageView>()
+    var handler:Handler=Handler()
+    var runnable:Runnable= Runnable {  }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,16 +24,42 @@ class MainActivity : AppCompatActivity() {
 
         score=0
 
+        imageArray= arrayListOf(imageView1,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9)
+
+        hideImages()
+
         object : CountDownTimer(10000,1000)
         {
             override fun onFinish() {
                 timeText.text="Time's Up"
+                handler.removeCallbacks(runnable)
+                for (image in imageArray)
+                {
+                    image.visibility=View.INVISIBLE
+                }
             }
 
             override fun onTick(millisUntilFinished: Long) {
                 timeText.text="Time: "+ millisUntilFinished/1000
             }
         }.start()
+    }
+
+    fun hideImages()
+    {
+        runnable=object :Runnable{
+            override fun run() {
+                for (image in imageArray)
+                {
+                    image.visibility=View.INVISIBLE
+                }
+                 var random=Random()
+                var index=random.nextInt(8-0)
+                imageArray[index].visibility=View.VISIBLE
+                handler.postDelayed(runnable,500)
+            }
+        }
+            handler.post(runnable)
     }
 
     fun increaseScore(view: View)
