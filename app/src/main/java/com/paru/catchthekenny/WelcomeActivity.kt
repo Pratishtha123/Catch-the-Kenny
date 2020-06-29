d9a2e898392b467a08ef01b3427f5f055da41943
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-06-29 21:29:29 +0530

    WelcomeActivity created

--- app/src/main/java/com/paru/catchthekenny/WelcomeActivity.kt
+++ app/src/main/java/com/paru/catchthekenny/WelcomeActivity.kt
@@ -1,12 +1,29 @@
 package com.paru.catchthekenny
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class WelcomeActivity : AppCompatActivity() {
+
+    lateinit var btnNew:Button
+    lateinit var btnExit:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        btnExit=findViewById(R.id.btnExit)
+        btnNew=findViewById(R.id.btnNew)
+
+        btnNew.setOnClickListener{
+            val intent=Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnExit.setOnClickListener{
+            finishAffinity()
+        }
     }
 }
