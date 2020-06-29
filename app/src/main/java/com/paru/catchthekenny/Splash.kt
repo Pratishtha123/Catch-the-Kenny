6dbaf6c9274910ecf4d6dc80b2769a08f0a82289
Pratishtha123 <54323039+Pratishtha123@users.noreply.github.com>
2020-06-29 20:39:35 +0530

    Splash activity created

---
+++ app/src/main/java/com/paru/catchthekenny/Splash.kt
@@ -0,0 +1,27 @@
+package com.paru.catchthekenny
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class Splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        ic_logo.startAnimation(AnimationUtils.loadAnimation(this,R.anim.splash_in))
+        Handler().postDelayed({
+            ic_logo.startAnimation(AnimationUtils.loadAnimation(this,R.anim.splash_out))
+            Handler().postDelayed({
+                ic_logo.visibility = View.GONE
+                startActivity(Intent(this,WelcomeActivity::class.java))
+                finish()
+            },1000)
+        },1500)
+    }
+}
